ac5a05ec2005a9397f321ca48615144e0f0434ac
facudobbo <37700877+facudobbo@users.noreply.github.com>
2018-04-23 15:13:12 -0500

    Se le agrego la prueba "assert" a los ej 1 y 2

--- Practica-01/Ejercicio-01.py
+++ Practica-01/Ejercicio-01.py
@@ -8,3 +8,6 @@ def maxi(a,b):
 
 x=maxi(9,4)
 print(x)
+
+assert maxi(9,4) == 9
+assert maxi (9,4)!= 4
--- Practica-01/Ejercicio-02.py
+++ Practica-01/Ejercicio-02.py
@@ -8,5 +8,8 @@ def max_de_tres(a,b,c):
     else:
         return c
 
-x=max_de_tres(6,5,6)
+x=max_de_tres(6,5,7)
 print(x)
+
+assert max_de_tres(6,5,7) == 7
+assert max_de_tres(6,5,7) != 6
