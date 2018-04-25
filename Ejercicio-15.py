e9161f85a4ce26bcace110143029f27b5beeea9c
carodeseta <38384495+carodeseta@users.noreply.github.com>
2018-04-25 11:01:30 -0300

    Update Ejercicio-15.py

--- Ejercicio-15.py
+++ Ejercicio-15.py
@@ -6,17 +6,12 @@ def programa():
     a=[]
     b=input("Ingrese un numero")
     while b != 'fin':
-        if b.isnumeric:
+        if b.isnumeric():
           a.append(b)
         b=input("Ingrese un numero")
     return a
 
 
-
-
-
 b=programa()
 print(max(b))
 print(min(b))
-
-
